e89e0261f1da0b01728d67ef8734633990e99682
Brian Okken <brian@variedthoughts.com>
2013-01-03 21:20:11 -0800

    add test_markdown_unittest.py

---
+++ test_markdown_unittest.py
@@ -0,0 +1,41 @@
+'''
+Test markdown.py with unittest
+To run tests:
+    python test_markdown_unittest.py
+'''
+
+import unittest
+from markdown_adapter import run_markdown
+
+class TestMarkdownPy(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_non_marked_lines(self):
+        '''
+        Non-marked lines should only get 'p' tags around all input
+        '''
+        self.assertEqual( 
+                run_markdown('this line has no special handling'), 
+                '<p>this line has no special handling</p>')
+
+    def test_em(self):
+        '''
+        Lines surrounded by asterisks should be wrapped in 'em' tags
+        '''
+        self.assertEqual( 
+                run_markdown('*this should be wrapped in em tags*'),
+                '<p><em>this should be wrapped in em tags</em></p>')
+
+    def test_strong(self):
+        '''
+        Lines surrounded by double asterisks should be wrapped in 'strong' tags
+        '''
+        self.assertEqual( 
+                run_markdown('**this should be wrapped in strong tags**'),
+                '<p><strong>this should be wrapped in strong tags</strong></p>')
+
+if __name__ == '__main__':
+    unittest.main()
+
