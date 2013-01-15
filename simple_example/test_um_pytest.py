d57716f38deee5f0ebf9aebddb5e2687373c237f
Brian Okken <brian@variedthoughts.com>
2013-01-15 05:29:46 -0800

    add pytest test files for both um and markdown

---
+++ simple_example/test_um_pytest.py
@@ -0,0 +1,18 @@
+'''
+Test unnecessary_math with pytest
+
+To run tests : py.test             test_um_pytest.py
+          or : python -m pytest    test_um_pytest.py
+Verobse (-v) : py.test -v          test_um_pytest.py
+          or : python -m pytest -v test_um_pytest.py
+'''
+
+from unnecessary_math import multiply
+
+def test_numbers_3_4():
+    assert ( multiply(3,4) == 12 )
+
+def test_strings_a_3():
+    assert ( multiply('a',3) == 'aaa' )
+
+
---
+++ test_markdown_pytest.py
@@ -0,0 +1,26 @@
+'''
+Test markdown.py with nose or pytest
+
+To run tests: py.test             test_markdown_pytest.py
+          or: python -m pytest    test_markdown_pytest.py
+Verbose (-v): py.test -v          test_markdown_pytest.py
+          or: python -m pytest -v test_markdown_pytest.py
+'''
+
+from markdown_adapter import run_markdown
+
+def test_non_marked_lines():
+    print ('in test_non_marked_lines')
+    assert ( run_markdown('this line has no special handling') == 
+            '<p>this line has no special handling</p>')
+
+def test_em():
+    print ('in test_em')
+    assert ( run_markdown('*this should be wrapped in em tags*') !=
+            '<p><em>this should be wrapped in em tags</em></p>')
+
+def test_strong():
+    print ('in test_strong')
+    assert ( run_markdown('**this should be wrapped in strong tags**') ==
+            '<p><strong>this should be wrapped in strong tags</strong></p>')
+
