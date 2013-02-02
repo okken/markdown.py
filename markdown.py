71f2acaaea1ca3ddee08c08d60fe06d3af69e29a
Brian Okken <brian@variedthoughts.com>
2013-02-02 13:16:24 -0800

    pass test_non_marked_lines() - wrap input in paragraph tags before spitting it back out

--- markdown.py
+++ markdown.py
@@ -1,13 +1,13 @@
 """
  Markdown.py
- First attempt
- - just print whatever is passed in to stdin
- - if filename passed in as a command line parameter, 
+ 0 just print whatever is passed in to stdin
+ 0 if filename passed in as a command line parameter, 
    then print file instead of stdin
+ 1 wrap input in paragraph tags
 """
 
 import fileinput
 
 for line in fileinput.input():
-    print line,  
+     print '<p>' + line + '</p>',
 
